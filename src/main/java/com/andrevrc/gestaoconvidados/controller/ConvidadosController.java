7b1c3e0fc49f80c2161d8725d7873c2c1f9732fb
André Carvalho <andrevrc@info.ufrn.br>
2021-03-05 14:12:42 -0300

    Adição de formulário

--- src/main/java/com/andrevrc/gestaoconvidados/controller/ConvidadosController.java
+++ src/main/java/com/andrevrc/gestaoconvidados/controller/ConvidadosController.java
@@ -1,24 +1,34 @@
 package com.andrevrc.gestaoconvidados.controller;
 
+import com.andrevrc.gestaoconvidados.model.Convidado;
 import com.andrevrc.gestaoconvidados.repository.Convidados;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 
     @Autowired
     private Convidados convidados;
 
-    @GetMapping("/convidados")
+    @GetMapping
     public ModelAndView listar() {
         ModelAndView modelAndView = new ModelAndView("ListaConvidados");
 
         modelAndView.addObject("convidados", convidados.findAll());
+        modelAndView.addObject(new Convidado());
 
         return modelAndView;
     }
 
+    @PostMapping
+    public String salvar(Convidado convidado) {
+        this.convidados.save(convidado);
+        return "redirect:/convidados";
+    }
 }
